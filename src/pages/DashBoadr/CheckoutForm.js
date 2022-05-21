23e7a98ff82403f4ab1b3b4c5984813817e1b369
Asadujjaman Julhas <fahim0760@gmail.com>
2022-05-21 13:23:30 +0600

    Setup payment page for get paid

---
+++ src/pages/DashBoadr/CheckoutForm.js
@@ -0,0 +1,63 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useState } from 'react';
+
+const CheckoutForm = ({ammount}) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [carderror, setCarderror] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card === null) {
+      return;
+    }
+ // Use your card Element with other Stripe.js APIs
+ const {error, paymentMethod} = await stripe.createPaymentMethod({
+  type: 'card',
+  card,
+});
+
+if (error) {
+  setCarderror(error.message);
+} else {
+  console.log('Hello 22')
+  console.log('[PaymentMethod]', paymentMethod);
+  setCarderror('')
+}
+    
+
+  };
+
+  return (
+    <div className='pt-5'>
+     <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: '16px',
+              color: '#424770',
+              '::placeholder': {
+                color: '#aab7c4',
+              },
+            },
+            invalid: {
+              color: '#9e2146',
+            },
+          },
+        }}
+      />
+      <button class="btn btn-sm btn-primary my-5  bg-gradient-to-r from-primary to-secondary"  type="submit" disabled={!stripe}>Pay Now ${ammount}</button>
+      <p className='text-[red] mb-5'>{carderror}</p>
+    </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
--- src/pages/DashBoadr/Payment.js
+++ src/pages/DashBoadr/Payment.js
@@ -1,9 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import PageTitle from '../../hooks/PageTitle';
 import LoadingSpinnerSmall from '../Shared/LoadingSpinnerSmall';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe('pk_test_51L1cHwAAX8jorUm97rVngApkylkvtoBRz8n8zNSHnNp5eSZ1CecJBkITNnU3OA5EDnTY4VQMF1QDyoDNgPHTL7Yk00hXgBEurM');
 const Payment = () => {
   const {id} = useParams();
 
@@ -28,10 +32,14 @@ if(isLoading) {
   <div class="card-body">
     <h2 class="card-title"><span className='text-orange-700'>${data.price}</span> Pay for {data.serviceName}</h2>
     <p>We will see you on <span className='text-orange-700'>{data.date}</span> at {data.slot}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
   </div>
+  <div class="card w-100 bg-orange-100 shadow-xl">
+  <div class="card-body  py-0">
+  <Elements stripe={stripePromise}>
+    <CheckoutForm ammount={data.price} />
+  </Elements>
+  </div>
+</div>
 </div>
     </div>
   );
